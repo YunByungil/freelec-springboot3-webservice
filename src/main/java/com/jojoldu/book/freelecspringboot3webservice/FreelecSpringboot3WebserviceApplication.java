442c5f51abba9c790e7b2c7b5a29f3cb51f9ee77
YunByungil <dbsquddlfz@naver.com>
2023-05-09 23:32:28 +0900

    프로젝트 생성

---
+++ src/main/java/com/jojoldu/book/freelecspringboot3webservice/FreelecSpringboot3WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot3webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot3WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringboot3WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/freelecspringboot3webservice/FreelecSpringboot3WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot3webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot3WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
