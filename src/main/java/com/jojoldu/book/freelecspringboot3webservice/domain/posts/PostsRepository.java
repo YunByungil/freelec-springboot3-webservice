e69f2ebbdbf8797dc374f2682887885dd48d594b
YunByungil <dbsquddlfz@naver.com>
2023-05-10 00:31:54 +0900

    refactor: 이름 변경, PostRepository -> PostsRepository

--- src/main/java/com/jojoldu/book/freelecspringboot3webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/freelecspringboot3webservice/domain/posts/PostsRepository.java
@@ -2,5 +2,5 @@ package com.jojoldu.book.freelecspringboot3webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Posts, Long> {
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
