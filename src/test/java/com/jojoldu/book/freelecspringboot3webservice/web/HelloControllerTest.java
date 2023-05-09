d04bbd0b1e113ce8ded88e0c9aee0c415715ce18
YunByungil <dbsquddlfz@naver.com>
2023-05-10 00:02:50 +0900

    test: JSON이 리턴되는 HelloController API 테스트

--- src/test/java/com/jojoldu/book/freelecspringboot3webservice/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot3webservice/web/HelloControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(controllers = HelloController.class)
 class HelloControllerTest {
@@ -25,5 +27,20 @@ class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @DisplayName("helloDto가 리턴된다")
+    @Test
+    void returnHelloDto() throws Exception {
+        String name = "hello";
+        int amount = 1_000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name) // String만 허용
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // JSON 응답값을 필드별로 검증할 수 있는 메서드
+                .andExpect(jsonPath("$.amount", is(amount))); // $를 기준으로 필드명을 명시
+
     }
 }
