b6d958e1fa705e87e269a57966c57ec2658c689d
YunByungil <dbsquddlfz@naver.com>
2023-05-12 22:16:14 +0900

    feat: IndexController, Index.Html, IndexControllerTest 추가

---
+++ src/test/java/com/jojoldu/book/freelecspringboot3webservice/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.jojoldu.book.freelecspringboot3webservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @DisplayName("메인페이지 로딩")
+    @Test
+    void loadIndexHtml() {
+        // given
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
