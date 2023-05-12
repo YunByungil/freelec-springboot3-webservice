cac04e609641855bc0c1fc22fbb5d18abcccad19
YunByungil <dbsquddlfz@naver.com>
2023-05-12 20:48:54 +0900

    feat: PostsService - Save 구현

---
+++ src/main/java/com/jojoldu/book/freelecspringboot3webservice/service/PostsService.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.freelecspringboot3webservice.service;
+
+import com.jojoldu.book.freelecspringboot3webservice.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
