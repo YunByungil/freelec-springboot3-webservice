bcf3e98815a9f1d926ff817b5825f1eda6bea9b3
YunByungil <dbsquddlfz@naver.com>
2023-05-12 21:47:46 +0900

    feat: BaseTimeEntity 테스트 코드 작성

--- src/test/java/com/jojoldu/book/freelecspringboot3webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot3webservice/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -47,6 +48,28 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @DisplayName("BaseTimeEntity 등록")
+    @Test
+    void addBaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
