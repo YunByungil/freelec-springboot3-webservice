bb5a063d03289035fb777997782a58c1f0830a63
YunByungil <dbsquddlfz@naver.com>
2023-05-14 21:09:51 +0900

    test: @WebMvcTest 문제 해결

--- src/test/java/com/jojoldu/book/freelecspringboot3webservice/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot3webservice/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jojoldu.book.freelecspringboot3webservice.web;
 
+import com.jojoldu.book.freelecspringboot3webservice.config.auth.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +17,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+                    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            })
 class HelloControllerTest {
 
     @Autowired
@@ -21,6 +28,7 @@ class HelloControllerTest {
 
     @DisplayName("hello가 리턴된다")
     @Test
+    @WithMockUser(roles = "USER")
     void returnHello() throws Exception {
         String hello = "hello";
 
@@ -31,6 +39,7 @@ class HelloControllerTest {
 
     @DisplayName("helloDto가 리턴된다")
     @Test
+    @WithMockUser(roles = "USER")
     void returnHelloDto() throws Exception {
         String name = "hello";
         int amount = 1_000;
